56b31702f37ca8ed57924c836c80be37cf1ba069
guansuo <2811060916@qq.com>
2022-09-07 19:45:51 +0800

    feat: 更新vite到3.0+版本

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import viteEslint from 'vite-plugin-eslint';
 import viteStylelint from '@amatlash/vite-plugin-stylelint';
 import svgLoader from 'vite-svg-loader';
 import path from 'path';
+import inspect from 'vite-plugin-inspect';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const CDN_URL = 'xxxxxx';
@@ -26,10 +27,17 @@ export default defineConfig({
     viteStylelint({
       // 对某些文件排除检查
       exclude: /windicss|node_modules/
-    })
+    }),
+    inspect()
   ],
   build: {
     // 8 KB
     assetsInlineLimit: 8 * 1024
+  },
+  optimizeDeps: {
+    // 将所有的 .vue 文件作为扫描入口
+    // entries: ['**/*.vue'],
+    // 配置为一个字符串数组，将 `lodash-es` 和 `vue`两个包强制进行预构建
+    // include: ['lodash-es', 'vue']
   }
 });
